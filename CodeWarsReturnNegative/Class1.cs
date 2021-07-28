0a993a18566ef96ecb6b927037fd3a3ac95f06da
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-28 14:01:20 -0600

    Add project files.

---
+++ CodeWarsReturnNegative/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace CodeWarsReturnNegative
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(-42, Kata.MakeNegative(42));
+        } 
+    }
+        
+
+public static class Kata
+    {
+        //In this simple assignment you are given a number and have to make it negative.
+        //But maybe the number is already negative?
+        public static int MakeNegative(int number)
+        {
+            //if (number < 0)
+            //{
+            //    return number;
+            //}
+            //else
+            //{
+            //    return number * -1;
+            //}
+            return -Math.Abs(number);
+
+        }
+    }
+}
